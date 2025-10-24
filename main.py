db537d0d62e88426657ff668babf1fdb2cc8debf
gsuspsus <gstok123@gmail.com>
2025-10-24 11:07:48 +0200

    cleaned up parsing logic and naming

--- main.py
+++ main.py
@@ -3,64 +3,82 @@ from pprint import pprint as prp
 import re
 import sys
 
-def define_header(text):
-    return pp.Keyword(text)
+def run_parse(p, t): return p.parse_string(t)
 
-def r(p, t): return p.parse_string(t)
+def cast_value(r): r[0][1] = bool(r[0][1])
 
-def must(p):
-    if not p: sys.exit()
-
-def cast_value(r):
-    print(r)
-    r[0][1] = bool(r[0][1])
-
-TEXT = define_header("TEXT")
-CHOICE = define_header("CHOICE")
-CHOICES = define_header("CHOICES")
-EFFECTS = define_header("EFFECTS")
-IF = define_header("IF")
-
-L_BRACE = pp.Suppress(pp.Literal("{"))
-R_BRACE = pp.Suppress(pp.Literal("}"))
-
-EMPTY_LINE = pp.LineStart() + pp.LineEnd()
-
-TEXT_BLOCK = pp.Group(TEXT + L_BRACE + pp.CharsNotIn("}").set_parse_action(pp.token_map(str.strip)) + R_BRACE)
-
-VAR_NAME = pp.Word(pp.alphas + "_")
-
-VARIABLE_DEFINITION = pp.Group(VAR_NAME + pp.Suppress("=") + pp.one_of("true", "false")).set_parse_action(cast_value)
-
-EFFECTS_BLOCK = pp.Group(pp.Suppress(EFFECTS) + L_BRACE + pp.Optional(pp.OneOrMore(VARIABLE_DEFINITION)) + pp.Optional(TEXT_BLOCK) + R_BRACE)
-
-CHOICE_DESC = pp.QuotedString("[", endQuoteChar="]", 
-                                 escChar="\\", 
-                                 multiline=True, 
-                                 unquoteResults=True).setResultsName("description")
-
-CHOICE_BLOCK = pp.Group(pp.Suppress(CHOICE) + L_BRACE + CHOICE_DESC + EFFECTS_BLOCK + pp.Optional(TEXT_BLOCK) + R_BRACE)
-
-CHOICES_BLOCK = pp.Group(CHOICES + L_BRACE + pp.Group(pp.OneOrMore(CHOICE_BLOCK)) + R_BRACE)
-
-ANY_BLOCK = CHOICES_BLOCK | TEXT_BLOCK
-
-IF_BLOCK = pp.Group(IF + VAR_NAME + L_BRACE + ANY_BLOCK + R_BRACE)
-
-DOCUMENT = pp.OneOrMore(ANY_BLOCK | IF_BLOCK)
-
-tokens = (r(DOCUMENT, open("test.wyd").read()).asList())
-
-print(tokens)
-
-world = {}
-choices = []
+def clean_text(r):
+    return "\n".join(filter(None, map(str.strip, r[0][0].split("\n"))))
 
 class Choice:
     def __init__(self, desc, vars, text):
         self.desc = desc
         self.vars = vars
         self.text = text
+    
+class ChoicesBlock:
+    def __init__(self, choices):
+        self.choices = choices[0]
+
+class TextBlock:
+    def __init__(self, text):
+        self.text = text
+    
+    def __str__(self):
+        return self.text
+
+class IfStatement:
+    def __init__(self, predicate, block):
+        self.predicate = predicate
+        self.block = block
+
+def cast_choice(r):
+    desc, [vars, text] = r[0]
+    return Choice(desc, dict([vars]), text)
+
+def cast_choices(r): return ChoicesBlock(r)
+
+def syntax_char(c): return pp.Suppress(pp.Literal(c))
+
+def cast_text_block(r): return TextBlock(clean_text(r))
+
+def cast_if_statement(r):
+    return IfStatement(r[0][0], r[0][1])
+
+L_BRACK = syntax_char("[")
+R_BRACK = syntax_char("]")
+L_BRACE = syntax_char("{")
+R_BRACE = syntax_char("}")
+EQUALS = syntax_char("=")
+
+IDENTIFIER = pp.Word(pp.alphas + "_")
+LABEL = pp.Optional(L_BRACK + IDENTIFIER + R_BRACK)
+VARIABLE_DEFINITION = pp.Group(IDENTIFIER + EQUALS + pp.one_of("true", "false")).set_parse_action(cast_value)
+
+def define_header(text): return pp.Suppress(pp.Keyword(text))
+
+def define_block(header, rules):
+    return pp.Group(define_header(header) + LABEL+ L_BRACE + rules + R_BRACE)
+
+TEXT_BLOCK = define_block("TEXT", pp.CharsNotIn("}")).set_parse_action(cast_text_block)
+EFFECTS_BLOCK = define_block("EFFECTS", pp.Optional(pp.OneOrMore(VARIABLE_DEFINITION)) + pp.Optional(TEXT_BLOCK))
+
+CHOICE_DESC = pp.QuotedString("[", endQuoteChar="]", 
+                                 escChar="\\", 
+                                 multiline=True, 
+                                 unquoteResults=True).setResultsName("description")
+
+CHOICE_BLOCK = define_block("CHOICE", CHOICE_DESC + EFFECTS_BLOCK + pp.Optional(TEXT_BLOCK)).set_parse_action(cast_choice)
+CHOICES_BLOCK = define_block("CHOICES", pp.OneOrMore(CHOICE_BLOCK)).set_parse_action(cast_choices)
+ANY_BLOCK = CHOICES_BLOCK | TEXT_BLOCK
+IF_STATEMENT = pp.Group(define_header("IF") + IDENTIFIER + L_BRACE + ANY_BLOCK + R_BRACE).set_parse_action(cast_if_statement)
+DOCUMENT = pp.OneOrMore(ANY_BLOCK | IF_STATEMENT)
+
+program = run_parse(DOCUMENT, open("test.wyd").read()).asList()
+print(program)
+
+world = {}
+choices = []
     
 def run_choices(cs):
     global world
@@ -71,31 +89,17 @@ def run_choices(cs):
     index = int(input())
     world |= cs[index].vars
 
-    print(cs[index].text)
-
     return cs[index].vars
 
-
-def run_tokens(tokens):
+def run_program(program):
     global world
-    for tok in tokens:
-        if tok[0] == "TEXT": print(tok[1])
-        elif tok[0] == "CHOICES":
-            for c in tok[1]:
-                desc = c[0]
-                effects = dict([c[1][0]])
-                text = c[1][1][1] if len(c[1]) > 1 else None
-                choices.append(Choice(desc,effects,text))
-            run_choices(choices)
-        elif tok[0] == "IF":
-            if world.get(tok[1], False):
-                run_tokens([tok[2]])
-    # while True: prp(r(EFFECTS, input()))
+    for tok in program:
+        if isinstance(tok, TextBlock): print(tok)
+        elif isinstance(tok, ChoicesBlock): run_choices(tok.choices)
+        elif isinstance(tok, IfStatement):
+            if world.get(tok.predicate, False):
+                run_program([tok.block])
 
-run_tokens(tokens)
+# while True: prp(r(EFFECTS, input()))
 
-"""
-IMPROVEMENTS:
-    ADD GOTO statement -- add labels
-    ADD Markdown support
-"""
+run_program(program)
